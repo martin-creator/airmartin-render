ebebfbebbd4331282df63f880363152737f22965
martin-creator <martinlubowa@outlook.com>
2022-08-24 08:13:58 +0300

    Formatted login in page using bootstrap 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :fullname, presense: true, length: {maximum: 50}
+
+  validates :fullname, presence: true, length: {maximum: 50}
 end
