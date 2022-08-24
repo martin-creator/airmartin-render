c4127b7e04cda6c97391d07325a432c44443957a
martin-creator <martinlubowa@outlook.com>
2022-08-24 07:28:21 +0300

    Added authentication with fullname

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :fullname, presense: true, length: {maximum: 50}
 end
