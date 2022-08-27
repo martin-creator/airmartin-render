a2bd61dad0824c2f8ac8a25a5802ed37090d614f
martin-creator <martinlubowa@outlook.com>
2022-08-27 16:58:35 +0300

    Created room controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:show]
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
 end
