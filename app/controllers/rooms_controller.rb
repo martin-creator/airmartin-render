981b72e0be9b45146b0fe283f3bc3998e39a86ed
martin-creator <martinlubowa@outlook.com>
2022-08-30 17:05:53 +0300

    Added server side verification for the publish button to prevent html attribute manipulation

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -43,7 +43,10 @@ class RoomsController < ApplicationController
   end
 
   def update
-    if @room.update(rooms_params)
+    new_params = room_params
+    new_params = room_params.merge(acrive: true) if is_ready_room
+
+    if @room.update(new_params)
       flash[:notice] = "Saved..."
     else
       flash[:alert] = "Something went wrong..."
@@ -58,6 +61,10 @@ class RoomsController < ApplicationController
 
     def is_authorised
       redirect_to root_path, alert: "You don't have permission" unless current_user.id == @room.user_id
+    end
+
+    def is_ready_room
+      @room.active && !@room.price.blank? && !@room.listing_name.blank?&& !@room.photos.blank?&& !@room.address.blank?
     end
 
     def rooms_params
