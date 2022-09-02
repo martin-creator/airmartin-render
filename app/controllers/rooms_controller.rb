a592db02589a9bfac337d2817a0e3dbc13b9e916
martin-creator <martinlubowa@outlook.com>
2022-09-02 18:54:59 +0300

    Added reservation status to listing and trips page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -60,7 +60,7 @@ class RoomsController < ApplicationController
 
   def preload
     today = Date.today
-    reservations = @room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+    reservations = @room.reservations.where("(start_date >= ? OR end_date >= ?) AND status = ?", today, today, 1)
 
     render json: reservations
   end
@@ -79,7 +79,7 @@ class RoomsController < ApplicationController
   private
 
     def is_conflict(start_date, end_date, room)
-      check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+      check = room.reservations.where("(? < start_date AND end_date < ?) AND status = ?", start_date, end_date, 1)
       check.size > 0? true : false
     end
 
