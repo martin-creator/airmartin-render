02da7596db04f9ecce476883e6082f112c40ed19
martin-creator <martinlubowa@outlook.com>
2022-09-03 06:01:58 +0300

    Created calendar availability model

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
 
   resources :guest_reviews, only: [:create, :destroy]
   resources :host_reviews, only: [:create, :destroy]
+  resources :calendars
 
   get '/your_trips' => 'reservations#your_trips'
   get '/your_reservations' => 'reservations#your_reservations'
