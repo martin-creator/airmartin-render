b8580011d8840ddf53010de80b6972c58ed97b6c
martin-creator <martinlubowa@outlook.com>
2022-09-09 22:08:56 +0300

    Added notification bell to nav bar

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,8 +1,7 @@
-class MessagesChannel < ApplicationCable::Channel
+
   class MessagesChannel < ApplicationCable::Channel
     def subscribed
       stream_from "conversation_#{params[:id]}"  
     end
   
   end
-end
